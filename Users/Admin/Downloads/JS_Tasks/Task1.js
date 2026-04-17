c7fc3a7eec261dbb83b2ff4386dc88cc39cd26d8
Rajarajan0608 <rajarajansp2003@gmail.com>
2026-04-17 16:33:38 +0530

    first commit

---
+++ Users/Admin/Downloads/JS_Tasks/Task1.js
@@ -0,0 +1,83 @@
+//  Task 1: Variable Update
+var num =50
+num =100
+console.log(num) // ans: 100
+
+
+// 🔹 Task 2: Let Variable Math
+let marks = 80
+marks = marks +10
+console.log(marks) //ans: 90
+
+
+// 🔹 Task 3: Const Value
+const price = 500
+var finalPrice = price + 100
+console.log(finalPrice)  // ans: 600
+
+
+// 🔹 Task 4: Printing Statements
+console.log("Welcome Team") // ans:Welcome Team
+console.log(2026) // ans: 2026
+console.log(true) // ans: true
+
+
+// 🔹 Task 5: Datatype Check
+console.log(typeof "JavaScript") // ans:string
+console.log(typeof 250)  // ans: number
+console.log(typeof false) // ans: boolean
+
+
+// 🔹 Task 6: Array Task
+let fruits = ["Apple","orange","Banana","Grapes","Mango"]
+
+console.log(fruits) // ans: ["Apple","orange","Banana","Grapes","Mango"]
+console.log(fruits[0]) // ans: Apple
+console.log(fruits[fruits.length-1]) // ans: Mango
+
+
+// 🔹 Task 7: Object Task
+ student = {
+ name: "Naveen",
+  age: 20,
+  course: "MERN"
+ }
+ console.log(student.name) // ans: Naveen
+ console.log(student.age) // ans: 20
+ console.log(student.course) // ans: MERN
+
+
+// 🔹 Task 8: Arithmetic Operators
+console.log(20 +10) // ans: 30
+console.log(50 - 25)// ans: 25
+console.log(5 * 5) // ans: 25
+console.log(100 / 4) // ans: 25
+console.log(20 % 3) // ans: 2
+
+
+// 🔹 Task 9: Increment / Decrement
+let x = 5
+x++
+console.log(x) // ans: 6
+let y = 10
+y--
+console.log(y) // ans: 9
+
+
+// 🔹 Task 10: Comparison Operators
+console.log(10 > 5)  // ans: true
+console.log(5 < 2)   // ans: false
+console.log(20 == "20")  // ans: true
+console.log(15 === "15")   // ans: false
+console.log(10 != 8)  // ans: true
+
+
+// 🔹 Task 11: Logical Operators
+console.log(5 > 2 && 10 > 3)  // ans: true
+console.log(7 < 5 || 8 > 2)   // ans: true
+console.log(!(10 > 5))   // ans: false
+
+
+// 🔹 Task 12: Ternary Operator
+let age = 18;
+console.log(age >= 18 ? "Eligible" : "Not Eligible");  // ans:Eligible
