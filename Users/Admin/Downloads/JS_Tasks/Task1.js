e4f3f3ae1332393382af356d86e72aef8d553a9b
Rajarajan0608 <rajarajansp2003@gmail.com>
2026-04-28 18:48:53 +0530

    Updated code

--- Users/Admin/Downloads/JS_Tasks/Task1.js
+++ Users/Admin/Downloads/JS_Tasks/Task1.js
@@ -239,4 +239,151 @@ else{
 // Login Success
 
 
+                                                //TASK - 3  28/04/2026
 
+//  Level 1 – Functions Basics
+//  Task 1: Student Form Function
+
+function studentForm(name, age, course){
+    console.log("Name: " + name)
+    console.log("Age: " + age)
+    console.log("Course: " + course)
+}
+studentForm("Raj",24,"ECE")
+
+// Task 2: Calculator Function
+
+function calc(a,b){
+    console.log("Addition: " + (a+b))
+    console.log("Subtraction: " + (a-b) )
+    console.log("Multiplication: " + (a*b))
+}
+calc(20,10)
+
+ // Task 3: Reusable Greeting
+
+ function Name(name){
+    console.log("Hello " + name )
+ }
+Name("Raj")
+Name("Kannan")
+Name("Kiru")
+
+ // Level 2 – Return & Scope
+// Task 4: Return Value
+
+function square(num){
+    return num*num
+}
+console.log(square(5))
+
+// Task 5: Scope Check
+
+function scope(){
+    let secret = "javascript"
+    console.log(secret)
+}
+// telescope()
+//console.log(secret) => error will occur because let variable works oly inside block
+
+// Level 3 – Spread / Rest
+// Task 6: Merge Arrays
+
+let boys = ["car","Bike"]
+let girls = ["doll","teddy"]
+let toys = [...boys, ...girls]
+console.log(toys)
+
+// Task 7: Unlimited Numbers
+
+function sumAll(...nums){
+    let total = 0
+
+     for (let i = 0; i < nums.length; i++) {
+        total += nums[i];
+    }
+
+    console.log(total)
+}
+sumAll(10, 20, 30, 40);
+
+// Level 4 – Destructuring
+// Task 8: Array Destructuring
+
+let colors =["red", "green", "blue"]
+let[c1,c2,c3] =  colors;
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+
+// Task 9: Object Destructuring
+
+let emp = {
+    name:"Raj",
+    role: "Developer",
+    salary: "5lpa"
+}
+let{name,role} = emp
+console.log(name,role)
+
+//Level 5 – Real-Time Logic
+// Task 10: Offer Generator
+
+function* offers(){
+yield "10% cashback"
+yield "20% cashback"
+yield "50% cashback"
+yield "Try again"
+}
+let offer = offers();
+
+console.log(offer.next().value)
+console.log(offer.next().value)
+console.log(offer.next().value)
+console.log(offer.next().value)
+
+let result = offer.next()
+if(result.done){
+    console.log("No more offers")
+}
+
+// Task 11: Curry Function
+
+function add(a){
+    return function(b){
+        return function(c){
+            return a + b +c
+        }
+    }
+}
+
+console.log(add(10)(20)(30))
+
+// Task 12: Student Marks Analyzer
+
+function mark(...nums) {
+    let total = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i];
+    }
+
+    let average = total / nums.length
+
+    console.log("Total: "+ total)
+    console.log("Average: " + average)
+}
+
+mark(80, 90, 70, 60)
+
+
+// Challenge Task
+
+function register(name, role, ...skills){
+    console.log("Name: " + name)
+    console.log("Role: " + role)
+    console.log("Skills: " + skills)
+}
+
+register("Raj","Developer","HTML","Css","Javascript","Java","SQL","SpringBoot")
